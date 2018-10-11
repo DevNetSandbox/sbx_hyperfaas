014bd6ce865017ae02e8fc1c3f9e61b6e34369ce
Andrii Omelianenko <andriy.omelianenko@gmail.com>
2018-10-11 12:26:20 +0300

    add hello number python func

--- basic-workflow/hello-number.py
+++ basic-workflow/hello-number.py
@@ -5,4 +5,4 @@ def main():
     if request.data:
         data = request.get_json()  # returns valid json
         number = data["number"]
-    return "Hello, %s!" % number
+    return "Hello, number %s, you are <50 and you are welcomed from Python function!\n" % number
