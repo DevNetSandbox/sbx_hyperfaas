d10059a56d21f6fecabc49a2fceec556811c8363
Andrii Omelianenko <andriy.omelianenko@gmail.com>
2018-10-11 11:58:38 +0300

    add generate number func

--- basic-workflow/generate-number.js
+++ basic-workflow/generate-number.js
@@ -1,8 +1,10 @@
 module.exports = async function (context) {
-    let random_number = Math.floor(Math.random() * 101);
+
+    let random_number = Math.floor(Math.random() * 101); // generate random number between 0 and 100
 
     return {
         status: 200,
-        body: "Hello, " + random_number + "!\n"
+        body: { random_number: random_number },
+        headers: { 'Content-Type': 'application/json' }
     };
 }
