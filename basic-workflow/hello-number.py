a7b144a75b0407f00192b9419ab54ab55a52f203
Andrii Omelianenko <andriy.omelianenko@gmail.com>
2018-10-11 12:27:56 +0300

    add hello number python func

--- basic-workflow/hello-number.py
+++ basic-workflow/hello-number.py
@@ -2,7 +2,9 @@ from flask import request
 
 
 def main():
+    number = 0
     if request.data:
         data = request.get_json()  # returns valid json
-        number = data["number"]
+        if "number"in data:
+            number = data["number"]
     return "Hello, number %s, you are <50 and you are welcomed from Python function!\n" % number
