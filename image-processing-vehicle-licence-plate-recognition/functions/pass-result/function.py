efd04944fe23290e7356bde4717722e37a11bf28
Andrii Omelianenko <andriy.omelianenko@gmail.com>
2018-11-27 15:22:52 +0200

    fix pass pict

--- image-processing-vehicle-licence-plate-recognition/functions/pass-result/function.py
+++ image-processing-vehicle-licence-plate-recognition/functions/pass-result/function.py
@@ -8,30 +8,38 @@ MONGODB_HOST = "mongo-db.hyperfaas.svc.cluster.local"
 MONGODB_PORT = 27017
 
 MONGODB_DATABASE = "image-processing"
-MONGODB_COLLECTION = "recognized-vehicle-license-plates"
+MONGODB_COLLECTION_RECOGNIZED = "recognized-plates"
+MONGODB_COLLECTION_NOT_RECOGNIZED = "not-recognized-plates"
 
 
 def main():
     if request.data:
         app.logger.info(request.get_json())
         if request.get_json():
-            recognized = request.get_json()["recognized"]
+            plate = request.get_json()["plate"]
 
-            if not recognized:
+            if not plate:
                 return "No content", 204
 
             mongo_url = "mongodb://{}".format(MONGODB_HOST)
 
             client = pymongo.MongoClient(mongo_url, MONGODB_PORT)
 
-            collection = client[MONGODB_DATABASE][MONGODB_COLLECTION]
-
-            insertion_object = {
-                "_id": request.get_json()["name"],
-                "plate": request.get_json()["plate"],
-                "recognized": request.get_json()["recognized"],
-                "confidence": request.get_json()["confidence"]
-            }
+            recognized = request.get_json()["recognized"]
+            if recognized:
+                collection = client[MONGODB_DATABASE][MONGODB_COLLECTION_RECOGNIZED]
+                insertion_object = {
+                    "_id": request.get_json()["name"],
+                    "plate": request.get_json()["plate"],
+                    "recognized": request.get_json()["recognized"],
+                    "confidence": request.get_json()["confidence"]
+                }
+            else:
+                collection = client[MONGODB_DATABASE][MONGODB_COLLECTION_NOT_RECOGNIZED]
+                insertion_object = {
+                    "_id": request.get_json()["name"],
+                    "plate": request.get_json()["plate"]
+                }
 
             try:
                 insert_one = collection.insert_one(insertion_object)
